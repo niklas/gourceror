c9ee46446c7687241756079fadcbdc3c60a9cbf3
Niklas Hofer <niklas+dev@lanpartei.de>
2012-08-29 17:36:48 +0200

    generated rails 3.2.8 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gourceror::Application.config.secret_token = '75e19ba0fc4efb3d38743e39b1591b01667eb624e45eadd559a75d0edd385aaa65eaaaec7eb627fcff7c6b31bfe9ca22b633da3528c6783bc96f8db44ede89c5'
